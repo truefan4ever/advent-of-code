f53a62f4f617568343c80079564c304dff3c2e11
k-tratseuski <k.tratseuski@gmail.com>
2022-12-06 10:39:21 +0300

    added day 6

---
+++ 2022/day_6/6_2.py
@@ -0,0 +1,20 @@
+def main():
+    with open('6.txt', 'r') as f:
+        data = f.read().split('\n')[0]
+        return find_start_marker(data)
+
+
+def find_start_marker(data: str) -> int:
+    buffer = []
+    for index, el in enumerate(data):
+        if len(buffer) == 14:
+            if len(set(buffer)) == 14:
+                # Since index in this sequense should be from 1 to ... 
+                return (index - 1) + 1
+            else:
+                buffer.pop(0)
+
+        buffer.append(el)
+
+if __name__ == "__main__":
+    print(main())
