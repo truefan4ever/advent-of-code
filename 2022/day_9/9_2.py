a56dc6bb462b6e2e8c3c3c78fa32d1f3eb3b1e52
truefan4ever <kotofey_98@mail.ru>
2022-12-12 02:00:08 +0300

    refactored day 9

--- 2022/day_9/9_2.py
+++ 2022/day_9/9_2.py
@@ -1,6 +1,3 @@
-from contextlib import suppress
-
-
 MOVES = {
     # y, x
     "L": (0, -1),
@@ -17,12 +14,16 @@ def main():
     return simulate_motions(data)
 
 
-def move_tail(h_position: tuple, t_position: tuple) -> tuple:
+def move_tail(h_position, t_position):
     x_axis_diff = abs(h_position[1] - t_position[1])
     y_axis_diff = abs(h_position[0] - t_position[0])
 
     if x_axis_diff <= 1 and y_axis_diff <= 1:
         return t_position
+
+    if y_axis_diff == 2 and x_axis_diff == 2:
+        # Use move tail along y and  x
+        return t_position[0] + 1 if h_position[0] > t_position[0] else t_position[0] - 1, t_position[1] + 1 if h_position[1] > t_position[1] else t_position[1] - 1
 
     if x_axis_diff == 2:
         # Use axis y from head and move tail along x
@@ -35,7 +36,7 @@ def move_tail(h_position: tuple, t_position: tuple) -> tuple:
 def simulate_motions(routes: list):
     # Start position
     current_h_position = (0, 0)
-    current_t_position = (0, 0)
+    current_t_position = [(0, 0) for _ in range(9)]
 
     tail_positions = set()
 
@@ -46,10 +47,19 @@ def simulate_motions(routes: list):
             # Set current head position
             current_h_position = (current_h_position[0] + y, current_h_position[1] + x)
 
-            # Set current tail position
-            current_t_position = move_tail(h_position=current_h_position, t_position=current_t_position)
+            # Set tail position with real head
+            current_t_position[0] = move_tail(
+                h_position=current_h_position,
+                t_position=current_t_position[0],
+            )
+            # Set tail position with real head as previous tail
+            for tail_id in range(1, 9):
+                current_t_position[tail_id] = move_tail(
+                    h_position=current_t_position[tail_id - 1],
+                    t_position=current_t_position[tail_id],
+                )
 
-            tail_positions.add(current_t_position)
+            tail_positions.add(current_t_position[-1])
 
     return len(tail_positions)
 
